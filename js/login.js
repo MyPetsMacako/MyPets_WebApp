16770e773e341ebed29035e4b3852cf08236656b
Iván Obejo <ivanobejo@hotmail.es>
2020-01-29 09:26:08 +0100

    Create users working

--- js/login.js
+++ js/login.js
@@ -23,7 +23,6 @@ function send (){
         type: "POST",
         url: "http://localhost:8888/laravel-ivanodp/MyPets_API/public/index.php/api/adminLogin",
         data: data,
-<<<<<<< HEAD:js/login.js
         success:function(response){
             //console.log(response["token"]);
             window.localStorage.setItem('token', response["token"]);
@@ -32,19 +31,6 @@ function send (){
         error: function(result) {
             document.getElementById('warning').style.display = 'block';
             document.getElementById('warning').innerHTML = result.responseJSON.message;
-=======
-        success: function(result)
-        {
-            if(result && result.auth_token.length>1) // you should do your checking here
-            {
-                window.location = 'http://www.google.com/'; //just to show that it went through
-            }
-            else
-            {
-                $('#result').empty().addClass('error')
-                    .append('Something is wrong.');
-            }
->>>>>>> e86478e9dee9e631f4378c7789744e28e606fd4c:app.js
         }
 
     });
